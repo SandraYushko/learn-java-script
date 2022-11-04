b4c41c0dd3dc11c772248d0c6e7723038ddf04a0
SandraYushko <shyria6@gmail.com>
2022-11-05 00:21:40 +0300

    if ... else

--- learn.js
+++ learn.js
@@ -1,4 +1,11 @@
-let i = 0;
-while (i < 3) {
-    alert( `number ${i++}!` );
-  }
+if (browser='Edge') {
+    alert("You've got the Edge!");  
+}
+else if (browser='Chrome', 'Firefox', 'Safari', 'Opera') {
+    alert( 'Okay we support these browsers too');
+}
+
+else {
+    alert('We hope that this page looks ok!');
+}
+      
