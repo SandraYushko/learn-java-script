9ca295ff48b8f847d94f28a5ab6f09f25872b540
SandraYushko <shyria6@gmail.com>
2022-11-05 14:56:25 +0300

    age

--- learn.js
+++ learn.js
@@ -1,15 +1,9 @@
-const number = +prompt('Введите число между 0 и 3', '');
-switch (number) {
-case 0:
-  alert('Вы ввели число 0');
-break;
-
-case 1:
-  alert('Вы ввели число 1');
-break;
-
-case 2:
-case 3:
-  alert('Вы ввели число 2, а может и 3');
-break;
-}     
+function checkAge(age) {
+    if (age > 18) {
+      return alert('true');
+    }
+    // ...
+    return confirm('Родители разрешили?');
+  } 
+  let age=prompt('введите ваш возраст');
+  checkAge(age) 
