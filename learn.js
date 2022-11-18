70c2e184deceab6d515fc695d729af5af260be01
SandraYushko <shyria6@gmail.com>
2022-11-18 23:24:12 +0300

    vse s nachala

--- learn.js
+++ learn.js
@@ -1,18 +1,1 @@
-var arrayOfSheep = [true,  true,  true,  false,
-  true,  true,  true,  true ,
-  true,  false, true,  false,
-  true,  false, false, true ,
-  true,  true,  true,  true ,
-  false, false, true,  true ];
-
-  function countSheeps(arrayOfSheep) {
-    // TODO May the force be with you
-    for (let i=0; i<arrayOfSheep.length; i++){
-      let k=0;
-      if (arrayOfSheep[i]===true) {k+=1}
-    }
-    return k
-  }
-
-
-  
+alert('Hello JS')
