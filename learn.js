ae3df02c8939d93113e7db53e9b000731216fdfa
SandraYushko <shyria6@gmail.com>
2022-11-20 16:30:50 +0300

    CodeWars. Convert a Number to a String!

--- learn.js
+++ learn.js
@@ -1,10 +1,4 @@
-function findAverage(array) {
-  // your code here
-  let length=array.length;
-  let summa=0;
-  
-  if (length==0) {return 0}
-  else {
-  array.forEach(function(elem) {summa+=elem});
-  return summa/length;}
+function numberToString(num) {
+  // Return a string of the number here!
+  return String(num);
 }
