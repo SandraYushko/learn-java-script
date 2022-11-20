42c52e946e7dc93ac540d9d3b959184f85ead151
SandraYushko <shyria6@gmail.com>
2022-11-20 23:36:48 +0300

    vozvedenie v kvadrat

--- learn.js
+++ learn.js
@@ -1,8 +1,4 @@
-let obj = {1: 'понедельник', 2: 'вторник', 3: 'среда', 4: 'четверг', 5: 'пятница', 6: 'суббота', 7: 'воскресенье'};
-console.log(obj[1]);
-console.log(obj[2]);
-console.log(obj[3]);
-console.log(obj[4]);
-console.log(obj[5]);
-console.log(obj[6]);
-console.log(obj[7]);
+let obj = {x: 1, y: 2, z: 3};
+obj['y']=2*2;
+obj['z']=3*3;
+console.log(obj);
