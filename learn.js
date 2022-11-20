be77e3a29f80fbb95752e685edffd87e70fe6f0a
SandraYushko <shyria6@gmail.com>
2022-11-20 17:38:32 +0300

    CodWars. Sum of positive

--- learn.js
+++ learn.js
@@ -1,4 +1,9 @@
-function cockroachSpeed(s) {
-  //Good Luck!
-  return parseInt(s*100000/3600);
-}
+function positiveSum(arr) {
+  let sum=0;
+  
+  for (let i=0; i<arr.length; i++) {
+    if (arr[i] >=0) sum += arr[i];
+   }
+    return sum;
+  
+  }
