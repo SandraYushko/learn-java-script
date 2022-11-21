51b7d223cf4ae4831206f7a0191e2ea4767e5188
SandraYushko <shyria6@gmail.com>
2022-11-21 21:46:07 +0300

    CodeWars. Function 1 - hello world

--- learn.js
+++ learn.js
@@ -1,9 +1,1 @@
-var countSheep = function (num){
-  //your code here
- let text='';
- for (i=1; i<=num;i++) {
-
-    text+=`${i}`+' sheep...';
-  }
-  return text;
-}
+function greet() {return "hello world!"}
