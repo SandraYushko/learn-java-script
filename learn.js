45dcba5df3c6831a5895f464ccbffc6396b42829
SandraYushko <shyria6@gmail.com>
2022-11-21 22:37:18 +0300

    CodeWars. Shortest Word

--- learn.js
+++ learn.js
@@ -1,1 +1,9 @@
-function greet() {return "hello world!"}
+function findShort(s) {
+  
+  let arr = s.split(' ');
+  let min=arr[0].length;
+     for (let i=1; i<(arr.length); i++) {
+        if (min>arr[i].length) {min=arr[i].length}
+     }
+  return min;
+}
