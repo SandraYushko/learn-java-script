66d8c3d41f337ca6e88d460d035040e7b9550746
SandraYushko <shyria6@gmail.com>
2022-11-22 22:48:09 +0300

    CodeWars. Remove String Spaces

--- learn.js
+++ learn.js
@@ -1,9 +1,4 @@
-function findShort(s) {
-  
-  let arr = s.split(' ');
-  let min=arr[0].length;
-     for (let i=1; i<(arr.length); i++) {
-        if (min>arr[i].length) {min=arr[i].length}
-     }
-  return min;
-}
+function noSpace(x) {
+  let newX = x.replace(/ /g,'');
+  return newX;
+  }
