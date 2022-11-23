3b226a31f4a7cbbfad90192ae4db83f82a50558f
SandraYushko <shyria6@gmail.com>
2022-11-23 22:38:38 +0300

    freeCodeCamp 1

--- learn.js
+++ learn.js
@@ -1,4 +1,11 @@
-function noSpace(x) {
-  let newX = x.replace(/ /g,'');
-  return newX;
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
   }
+}
+
+orderMyLogic(7);
