e5dcc6fe43ada74497d68e671700a49338ac76b2
SandraYushko <shyria6@gmail.com>
2022-11-23 22:56:17 +0300

    freeCodeCamp 3

--- learn.js
+++ learn.js
@@ -1,20 +1,18 @@
-function testSize(num) {
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
   // Only change code below this line
-if (num < 5) {
-  return 'Tiny'
-} else if (num < 10) {
-  return 'Small'
-} else if (num < 15) {
-  return 'Medium'
-} else if (num < 20) {
-  return 'Large'
+if (strokes==1) {return names[0]}
+else if (strokes<= par - 2) {return names[1]}
 
-} else if (num >= 20) {
-  return 'Huge'}
+else if (strokes==par - 1) {return names[2]}
+else if (strokes==par) {return names[3]}
+else if (strokes==par + 1) {return names[4]}
+else if (strokes==par + 2) {return names[5]}
+else if (strokes>= par + 3) {return names[6]}
 
-  else{
-  return "Change Me";}
+  else {return "Change Me";}
   // Only change code above this line
 }
 
-testSize(7);
+golfScore(5, 4);
