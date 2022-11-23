68e7979c66505fbc5d8740e3bff69850198a5da6
SandraYushko <shyria6@gmail.com>
2022-11-23 23:12:49 +0300

    freeCodeCamp 5

--- learn.js
+++ learn.js
@@ -1,24 +1,24 @@
-function caseInSwitch(val) {
+function switchOfStuff(val) {
   let answer = "";
   // Only change code below this line
+
 switch (val) {
-  case 1:
-    return "alpha";
+  case 'a':
+    return "apple";
     break;
-  case 2:
-    return "beta";
+  case 'b':
+    return "bird";
     break;
-  case 3:
-    return "gamma";
+  case 'c':
+    return "cat";
     break;
-  case 4:
-    return "delta";
+  default:
+    return "stuff";
     break;
 }
-
 
   // Only change code above this line
   return answer;
 }
 
-caseInSwitch(1);
+switchOfStuff(1);
