ee2f83e0b1912402bf256cb0704a7bcf5370f3fd
SandraYushko <shyria6@gmail.com>
2022-12-01 23:59:20 +0300

    freeCodeCamp 8-recursia

--- learn.js
+++ learn.js
@@ -1,6 +1,10 @@
-function factorial(n) {
-  if (n == 1) return 1;
-  return n * factorial(n-1);
+function sum(arr, n) {
+  // Only change code below this line
+ if (n < 1) {
+      return 0;
+    } else {
+      return sum(arr, n - 1) + arr[n - 1];
+    }
+  
+  // Only change code above this line
 }
-
-alert( factorial(5) ); // 120
