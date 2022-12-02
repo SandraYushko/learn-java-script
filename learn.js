0039cf882bf1767cbea03503f12ef030aa59b985
SandraYushko <shyria6@gmail.com>
2022-12-02 23:31:01 +0300

    recursia

--- learn.js
+++ learn.js
@@ -1,10 +1,10 @@
-function sum(arr, n) {
-  // Only change code below this line
- if (n < 1) {
-      return 0;
-    } else {
-      return sum(arr, n - 1) + arr[n - 1];
-    }
-  
-  // Only change code above this line
+function pow (x, n) {
+  if (n==1) {
+    return x;
+  }
+  else {
+    return x*pow(x,n-1);
+  }
 }
+
+console.log(pow(2,3));
