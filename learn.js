1460bd5de9f56c72287bd42673c9936e300d4629
SandraYushko <shyria6@gmail.com>
2022-12-03 18:38:51 +0300

    codeWars. Sum Arrays

--- learn.js
+++ learn.js
@@ -1,4 +1,9 @@
-function solution(str){
-  
-  return str.split("").reverse().join("");
-   }
+// Sum Numbers
+function sum (numbers) {
+  "use strict";
+  let sum=0;
+  numbers.forEach(function(elem) {
+  sum+=elem; 
+  });
+  return sum;
+};
