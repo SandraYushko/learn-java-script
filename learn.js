f3fddcf28e455c395f35a05601769f4720f0addc
SandraYushko <shyria6@gmail.com>
2022-12-03 13:59:14 +0300

    codeWars. Opposite number

--- learn.js
+++ learn.js
@@ -1,10 +1,4 @@
-function pow (x, n) {
-  if (n==1) {
-    return x;
-  }
-  else {
-    return x*pow(x,n-1);
-  }
+function opposite(number) {
+  //your code here
+  return number*(-1)
 }
-
-console.log(pow(2,3));
