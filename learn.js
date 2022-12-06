aeeb5afe02159b51dc5dc0cbf8d0728092a0bd44
SandraYushko <shyria6@gmail.com>
2022-12-06 23:42:43 +0300

    code.mu. vlojennie if

--- learn.js
+++ learn.js
@@ -1,5 +1,14 @@
-class SmallestIntegerFinder {
-  findSmallestInt(args) {
-  return Math.min.apply(null,args);  
-  }
-}
+let num = 56
+
+	if (num >= 10 && num <= 99) {
+		let num1=String(num);
+    num1=Number(num1[0])+Number(num1[1]);
+    console.log(num1);
+    if (num1<=9) {
+      console.log('summa odnoznachna');
+    }
+    else {
+      console.log('summa dvuznachna');
+    }
+	} 
+  
