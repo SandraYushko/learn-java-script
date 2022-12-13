fc86cf5241df34750464f63462b0df754cc36ca5
SandraYushko <shyria6@gmail.com>
2022-12-13 23:19:55 +0300

    CodeWars. Correct the mistakes of the character recognition software

--- learn.js
+++ learn.js
@@ -1,8 +1,16 @@
 
-function getAge(inputString){
-  // return the girl's correct age as an integer. Happy coding :) 
-    return parseInt(inputString);
-  }
+function correct(string)
+{
+	// your code here
+let n1=/0/gi;
+let n2=/5/gi;
+let n3=/1/gi;    
+  let newStr=string.replace(n2, 'S'); 
+  newStr=newStr.replace(n1, 'O'); 
+  newStr=newStr.replace(n3, 'I'); 
+  return newStr;
+  
+}
 
 
 
