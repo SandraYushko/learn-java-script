579eaff520fcdb29688785c79ccfde7be9a7fbbe
SandraYushko <shyria6@gmail.com>
2022-12-13 23:37:26 +0300

    CodeWars. Quarter of the year

--- learn.js
+++ learn.js
@@ -1,16 +1,24 @@
-
-function correct(string)
-{
-	// your code here
-let n1=/0/gi;
-let n2=/5/gi;
-let n3=/1/gi;    
-  let newStr=string.replace(n2, 'S'); 
-  newStr=newStr.replace(n1, 'O'); 
-  newStr=newStr.replace(n3, 'I'); 
-  return newStr;
-  
+const quarterOf = (month) => {
+  // Your code here
+  switch (month) {
+      case 1:
+      case 2:
+      case 3:
+      return 1
+      break;
+      case 4:
+      case 5:
+      case 6:
+      return 2
+      break;
+      case 7:
+      case 8:
+      case 9:
+      return 3
+      break;
+      case 10:
+      case 11:
+      case 12:
+      return 4
+      break; 
 }
-
-
-
