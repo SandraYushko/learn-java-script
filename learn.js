ea18815f1ce5d9e97174ef2ef9bffa82ce32e506
SandraYushko <shyria6@gmail.com>
2022-12-14 22:24:36 +0300

    CodeWars. Get the mean of an array

--- learn.js
+++ learn.js
@@ -1,7 +1,7 @@
-function arrayPlusArray(arr1, arr2) {
-  let newArr=arr1.concat(arr2);
-  let result=newArr.reduce(function(sum,elem){
+function getAverage(marks){
+  //TODO : calculate the downward rounded average of the marks array
+  let itog=marks.reduce(function(sum,elem){
     return sum+elem;
   },0);
-  return result;
+  return Math.floor(itog/marks.length);
 }
