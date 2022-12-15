60f4b925b02ade7a2951e966ee9ecfb6b1048a37
SandraYushko <shyria6@gmail.com>
2022-12-15 23:46:33 +0300

    CodeWars. Fake Binary

--- learn.js
+++ learn.js
@@ -1,4 +1,3 @@
-let str = `a
-b
-c`; // так будет работать
-console.log(str)
+function fakeBin(x){
+  return x.replace(/1/gi,'0').replace(/2/gi,'0').replace(/3/gi,'0').replace(/4/gi,'0').replace(/9/gi,'1').replace(/8/gi,'1').replace(/5/gi,'1').replace(/6/gi,'1').replace(/7/g,'1')
+  }
