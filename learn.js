701ce389f0374b73c3040c6e5a090115fed3cad0
SandraYushko <shyria6@gmail.com>
2022-12-19 23:54:22 +0300

    CodeWars. Simple multiplication

--- learn.js
+++ learn.js
@@ -1,6 +1,5 @@
-function areYouPlayingBanjo(name) {
-  // Implement me
-  if (name[0]=='R'||name[0]=='r')
-  {return name + " plays banjo";}
-  else {return name + " does not play banjo";}
+function simpleMultiplication(number) {
+  // your code........
+return (number%2)? number*9 : number*8;
+
 }
