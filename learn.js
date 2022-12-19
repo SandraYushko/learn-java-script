666838d16f66c158179c30bcbcd58ea3e92ff0e0
SandraYushko <shyria6@gmail.com>
2022-12-19 23:42:59 +0300

    CodeWars. Are You Playing Banjo?

--- learn.js
+++ learn.js
@@ -1,3 +1,6 @@
-function fakeBin(x){
-  return x.replace(/1/gi,'0').replace(/2/gi,'0').replace(/3/gi,'0').replace(/4/gi,'0').replace(/9/gi,'1').replace(/8/gi,'1').replace(/5/gi,'1').replace(/6/gi,'1').replace(/7/g,'1')
-  }
+function areYouPlayingBanjo(name) {
+  // Implement me
+  if (name[0]=='R'||name[0]=='r')
+  {return name + " plays banjo";}
+  else {return name + " does not play banjo";}
+}
