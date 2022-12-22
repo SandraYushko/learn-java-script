652abcb894f787ee2a1d34e589463d2404a26ec8
SandraYushko <shyria6@gmail.com>
2022-12-22 22:28:43 +0300

    CodeWars. Calculate BMI

--- learn.js
+++ learn.js
@@ -1,4 +1,12 @@
-const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
-  // TODO
-  return distanceToPump/mpg<=fuelLeft;
-};
+function bmi(weight, height) {
+  let bmi=weight/height/height;
+  
+  if (bmi<= 18.5) {
+        return "Underweight"}
+  else if (bmi<= 25.0) {
+        return "Normal"}
+   else if (bmi<= 30.0) {
+        return "Overweight"}     
+   else {return "Obese"}  
+    
+}
