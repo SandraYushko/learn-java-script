787b768bf7811669a183b1c2dc38777e85fbdeef
SandraYushko <shyria6@gmail.com>
2022-12-22 22:55:32 +0300

    CodeWars. Returning Strings

--- learn.js
+++ learn.js
@@ -1,12 +1,4 @@
-function bmi(weight, height) {
-  let bmi=weight/height/height;
-  
-  if (bmi<= 18.5) {
-        return "Underweight"}
-  else if (bmi<= 25.0) {
-        return "Normal"}
-   else if (bmi<= 30.0) {
-        return "Overweight"}     
-   else {return "Obese"}  
-    
+function greet(name){
+  //your code here
+  return `Hello, ${name} how are you doing today?`
 }
