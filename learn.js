eaed37d141da60224db05fe4fa42b6a6480e892e
SandraYushko <shyria6@gmail.com>
2022-12-25 18:04:03 +0300

    learn.js. Привет, object

--- learn.js
+++ learn.js
@@ -1,4 +1,6 @@
-function booleanToString(b){
-  //your code here
-  return String(b);
-}
+let user={};
+user.name='John';
+user.surname='Smith';
+user.name='Pete';
+delete user.name;
+console.log(user);
