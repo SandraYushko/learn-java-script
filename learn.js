c90b0f53cae3bb586a539d5a291e5fc3bb75c4a1
SandraYushko <shyria6@gmail.com>
2022-12-25 19:35:33 +0300

    learn.js. Сумма свойств объекта

--- learn.js
+++ learn.js
@@ -1,6 +1,11 @@
-let user={};
-user.name='John';
-user.surname='Smith';
-user.name='Pete';
-delete user.name;
-console.log(user);
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+
+let sum=0;
+for (let key in salaries) {
+  sum+=salaries[key];
+}
+console.log(sum);
