2b2ca9177aadc4f06e9dcec6f1abd358c1cd8aa2
SandraYushko <shyria6@gmail.com>
2022-12-25 20:07:43 +0300

    CodeWars. Cat years, Dog years

--- learn.js
+++ learn.js
@@ -1,15 +1,20 @@
-let menu = {
-  width: 200,
-  height: 300,
-  title: "My menu"
-};
-
-function multiplyNumeric(menu) {
-  for (let key in menu) {
-    if (typeof menu[key] == 'number') {
-      menu[key] *= 2;
-    }
+var humanYearsCatYearsDogYears = function(humanYears) {
+  // Your code here!
+  let CatYears=0;
+  let DogYears=0;
+  
+  if (humanYears==1) {
+    CatYears+=15;
+    DogYears+=15;
   }
+  else if (humanYears==2) {
+    CatYears+=15+9;
+    DogYears+=15+9;
+  }
+   else {
+    CatYears+=15+9+(humanYears-2)*4;
+    DogYears+=15+9+(humanYears-2)*5;
+  }
+  
+  return [humanYears,CatYears,DogYears];
 }
-multiplyNumeric(menu);
-console.log(menu);
