c8ca38acaf10d059253c8e50eba1cfc5f2dd8994
SandraYushko <shyria6@gmail.com>
2023-02-12 22:42:11 +0300

    learn.js
    stroki

--- learn.js
+++ learn.js
@@ -1,6 +1,7 @@
-function barTriang(p1, p2, p3){
-  //your code here
-  let x0 = +((p1[0] + p2[0] + p3[0])/3).toFixed(4);
-  let y0 = +((p1[1] + p2[1] + p3[1])/3).toFixed(4);
-  return [x0, y0];
+function ucFirst(str) {
+  if (!str) return str;
+
+  return str[0].toUpperCase() + str.slice(1);
 }
+
+alert( ucFirst("вася") );
