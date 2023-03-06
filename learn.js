18adaf33b560fdda9bb731d19df47992d05e255b
SandraYushko <shyria6@gmail.com>
2023-03-06 23:23:33 +0300

    code.wars 06/03/2023

--- learn.js
+++ learn.js
@@ -1,24 +1,7 @@
-/*function sortByAge(arr) {
-  arr.sort((a, b) => a.age > b.age ? 1 : -1);
-}
-*/
-
-function sortByAge(arr) {
-  arr.sort((a, b) => {return a.age - b.age});
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+  return l.filter(item => typeof item==='number');
+   
 }
 
-
-let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 28 };
-
-let arr = [ vasya, petya, masha ];
-
-sortByAge(arr);
-
-// теперь: [vasya, masha, petya]
-alert(arr[0].name); // Вася
-alert(arr[1].name); // Маша
-alert(arr[2].name); // Петя
-
-
+filter_list([1,2,'a','b']) 
