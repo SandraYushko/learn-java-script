1cc897d8927fcd80b8a320c1e13eb0afc7c6a08a
SandraYushko <shyria6@gmail.com>
2023-03-06 23:41:00 +0300

    code.wars - Training JS #7: if..else and ternary operator

--- learn.js
+++ learn.js
@@ -1,4 +1,5 @@
-// Write the "square"-function here
-function square(x){
-  return x*x;
+function saleHotdogs(n){
+  if (n<5) return n*100;
+  else if (n>=5 && n<10) return n*95;
+  else return n*90;
 }
