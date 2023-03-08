62f8a263074609be34b5f148ddef363eef128c2e
SandraYushko <shyria6@gmail.com>
2023-03-08 23:08:49 +0300

    learn.js - 08/03/2023

--- learn.js
+++ learn.js
@@ -1,1 +1,15 @@
-var countSquares = cuts => (cuts==1) ? 1 : 6*cuts*cuts+2;
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 29 };
+
+let arr = [ vasya, petya, masha ];
+let sum=0;
+function getAverageAge(arr) {
+  arr.forEach(function(elem){
+    sum+=elem.age;
+  });
+  return sum/arr.length;  
+};
+
+
+alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
