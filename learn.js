befb66a5fb581c3f74bc9c1545a388a027cb0ed6
SandraYushko <shyria6@gmail.com>
2023-03-09 23:47:25 +0300

    learn.js 09/03/2023

--- learn.js
+++ learn.js
@@ -1,16 +1,23 @@
-function unique(arr) {
-  /* ваш код */
-  let result = [];
-  for (let i=0; i<arr.length; i++) {
-    if (!result.includes(arr[i])) {
-      result.push(arr[i]);
-    }
-  }
-  alert (result);
-}
-
-let strings = ["кришна", "кришна", "харе", "харе",
-  "харе", "харе", "кришна", "кришна", ":-O"
+let users = [
+  {id: 'john', name: "John Smith", age: 20},
+  {id: 'ann', name: "Ann Smith", age: 24},
+  {id: 'pete', name: "Pete Peterson", age: 31},
 ];
 
-alert( unique(strings) ); // кришна, харе, :-O
+function groupById(array) {
+  return array.reduce((obj, value) => {
+    obj[value.id] = value;
+    return obj;
+  }, {})
+}
+
+let usersById = groupById(users);
+/*
+// после вызова у нас должно получиться:
+usersById = {
+  john: {id: 'john', name: "John Smith", age: 20},
+  ann: {id: 'ann', name: "Ann Smith", age: 24},
+  pete: {id: 'pete', name: "Pete Peterson", age: 31},
+}
+*/
+
