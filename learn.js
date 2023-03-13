f67b407d6ab8184154ad8d57f8c3e046bddaa116
SandraYushko <shyria6@gmail.com>
2023-03-13 23:37:06 +0300

    code wars - Build a square
    Build a square

--- learn.js
+++ learn.js
@@ -1,17 +1,5 @@
-function generateShape(integer){
-  let str='';
-  for (let i=0; i<integer; i++){
-    for (let j=0; j<integer; j++){
-      str += "+";
-    }
-   if (i<integer-1) {str += "\n"}; 
-    
-  }
-return str;    
+function generateShape(n){
+  return ("+".repeat(n)+"\n").repeat(n).trim()
 }
-
-
-
-Build a square
 
 
