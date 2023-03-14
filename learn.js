36dc3c33e03cfa26453a204fb710243df9c3f3c5
SandraYushko <shyria6@gmail.com>
2023-03-14 22:22:12 +0300

    code wars 14/03/2023
    Training JS #1: create your first JS function and print "Hello World!"

--- learn.js
+++ learn.js
@@ -1,9 +1,7 @@
-function isPythagoreanTriple(integers) {
-  // Good luck friends!
-let integers1 = integers.sort(function(a,b){return a-b});
-if (integers1[0]*integers1[0] + integers1[1]*integers1[1] == integers1[2]*integers1[2]) {return true}
-  else {return false}
+//refer to the example and write your first JS function
+function helloWorld(){
+  let str = "Hello World!";
+  console.log(str);
 }
-
-isPythagoreanTriple([3, 4, 5]);
+helloWorld();
 
