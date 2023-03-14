a5922fcd50648ae102e3df3e7dbc3d6d086bec88
SandraYushko <shyria6@gmail.com>
2023-03-14 21:52:05 +0300

    code wars 14/03/2023
    Vowel Count

--- learn.js
+++ learn.js
@@ -1,5 +1,13 @@
-function generateShape(n){
-  return ("+".repeat(n)+"\n").repeat(n).trim()
-}
+function getCount(str) {
+    let sumGlasnih=0;
+    let str2=[...str];
+    str2.forEach(function(elem){
+      if ((elem=="a") || (elem=="e") || (elem=="i") || (elem=="o") || (elem=="u") )
+      {sumGlasnih+=1;}
+    });
 
+    return sumGlasnih;
+  }
+   
+getCount('abracadabra');
 
