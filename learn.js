dd7b868904689c88bde76720821424e474f5aee9
SandraYushko <shyria6@gmail.com>
2023-03-16 23:23:46 +0300

    CodeWars 16.03.2023
    Disemvowel Trolls

--- learn.js
+++ learn.js
@@ -1,4 +1,7 @@
-function shortcut (string) {
-  return string.replace(/a/g,'').replace(/e/g,'').replace(/i/g,'').replace(/o/g,'').replace(/u/g,'');
+function disemvowel(str) {
+  return str.replace(/a/g,'').replace(/e/g,'').replace(/i/g,'').replace(/o/g,'').replace(/u/g,'').replace(/A/g,'').replace(/E/g,'').replace(/I/g,'').replace(/O/g,'').replace(/U/g,'');
 }
+
+
+
 
