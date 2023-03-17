7e637fbe353ca89e4a189d96bdeba7c3d4e5a976
SandraYushko <shyria6@gmail.com>
2023-03-18 00:25:21 +0300

    Code Wars 18/03/2023
    Total amount of points

--- learn.js
+++ learn.js
@@ -1,7 +1,12 @@
-function otherAngle(a, b) {
-  return (180-a-b);
-}
-
+function points(games) {
+  let sum = 0;
+  games.forEach(item => {
+    if (item[0] > item[2])  sum+=3;
+    if (item[0] === item[2])  sum+=1;
+  });
+  return sum;
+  
+  }
 
 
 
