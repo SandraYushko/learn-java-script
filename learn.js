e630d9f4f3a116d2c5b302316b1c044dc472edbf
SandraYushko <shyria6@gmail.com>
2023-03-18 00:31:08 +0300

    CodeWars 18/03/2023
    7 kyu
    String ends with?

--- learn.js
+++ learn.js
@@ -1,12 +1,7 @@
-function points(games) {
-  let sum = 0;
-  games.forEach(item => {
-    if (item[0] > item[2])  sum+=3;
-    if (item[0] === item[2])  sum+=1;
-  });
-  return sum;
-  
-  }
+function solution(str, ending){
+  // TODO: complete
+  return str.endsWith(ending);
+}
 
 
 
