49fe6b0f778b9c172eb3c8cf80277836d6c73be1
SandraYushko <shyria6@gmail.com>
2023-03-17 23:27:29 +0300

    codeWars 17/03/2023
    Double Char

--- learn.js
+++ learn.js
@@ -1,6 +1,8 @@
-function disemvowel(str) {
-  return str.replace(/a/g,'').replace(/e/g,'').replace(/i/g,'').replace(/o/g,'').replace(/u/g,'').replace(/A/g,'').replace(/E/g,'').replace(/I/g,'').replace(/O/g,'').replace(/U/g,'');
+function doubleChar(str) {
+  // Your code here
+return str.split("").map(el => el+el).join("");
 }
+
 
 
 
