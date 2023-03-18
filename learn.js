ae8357d8f856bd8ef089fe21ed39b5f9827308b6
SandraYushko <shyria6@gmail.com>
2023-03-18 12:02:48 +0300

    CodeWars 18/03/2023
    7 kyu
    Count the divisors of a number

--- learn.js
+++ learn.js
@@ -1,6 +1,10 @@
-function solution(str, ending){
-  // TODO: complete
-  return str.endsWith(ending);
+function getDivisorsCnt(n){
+  // todo
+let sum = 0;
+for (let i=1; i<=n; i++){
+  if (n%i == 0) sum += 1
+}
+return sum;
 }
 
 
