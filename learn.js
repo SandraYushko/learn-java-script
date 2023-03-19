1bb450ed8a242d3080768e0f4038320532d59b18
SandraYushko <shyria6@gmail.com>
2023-03-19 19:17:35 +0300

    video Bogdan

--- learn.js
+++ learn.js
@@ -1,8 +1,19 @@
-function addBinary(a,b) {
-  let num = a+b;
-  return num.toString(2);
+class Comment {
+  constructor (text){
+    this.text=text
+    this.votesQty = 0
+  }
+    
+  upvote(){
+    this.votesQty +=1
+  }
+
+  static mergeComments(first, second){
+      return `${first} ${second}`
 }
 
+const firstComment = new Comment("First comment")
+const secondComment = new Comment("Second comment")
 
 
 
