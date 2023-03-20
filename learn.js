efaf01c62c91630798f5eeeb44686692854ccbc8
SandraYushko <shyria6@gmail.com>
2023-03-20 23:21:12 +0300

    CodeWars 20/03/2023
    8 kyu
    Is it even?

--- learn.js
+++ learn.js
@@ -1,12 +1,7 @@
-var number = function(busStops){
-  // Good Luck!
-  let sum = 0;
-  let numberOfPeople = 0;
-  for (const key in busStops){
-    sum = busStops[key][0] - busStops[key][1];
-    numberOfPeople += sum; 
-  }
-  return numberOfPeople;
+function testEven(n) {
+  //Your awesome code here!
+if (n%2 ==0) return true
+else return false
 }
 
 
