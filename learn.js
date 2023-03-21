474ef38cd568c33f52525ace8508b5bd198f386e
SandraYushko <shyria6@gmail.com>
2023-03-21 22:14:03 +0300

    CodeWars 21.03.2023
    7 kyu
    Growth of a Population

--- learn.js
+++ learn.js
@@ -1,5 +1,9 @@
-function minMax(arr){
-  return [Math.min.apply(null,arr),Math.max.apply(null,arr)]; // fix me!
+function nbYear(p0, percent, aug, p) {
+  let years = 0;
+  for (years; p0 < p; years++) {
+    p0 += p0 * (percent / 100) + aug;
+  }
+  return years;
 }
 
 
