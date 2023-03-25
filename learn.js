520af92dd018b7b33415d6396d3e0c381cdc15ff
SandraYushko <shyria6@gmail.com>
2023-03-25 11:57:54 +0300

    codewars 25/03/2023
    7 kyu
    Jaden Casing Strings

--- learn.js
+++ learn.js
@@ -1,7 +1,10 @@
-function twoSort(s) {
-  let massiv = s.sort();
-  return massiv[0].split("").join('***');
-  }
+String.prototype.toJadenCase = function () {
+  //...
+  return this
+  .split(" ")
+  .map(word => (word[0].toUpperCase() + word.slice(1)))
+  .join(" ");
+};
 
 
 
