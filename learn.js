7a0e335cd69e06b1e758ef519a1b85ac1badf3a2
SandraYushko <shyria6@gmail.com>
2023-03-27 22:34:27 +0300

    DOM 27.03.2023

--- learn.js
+++ learn.js
@@ -1,10 +1,5 @@
-String.prototype.toJadenCase = function () {
-  //...
-  return this
-  .split(" ")
-  .map(word => (word[0].toUpperCase() + word.slice(1)))
-  .join(" ");
-};
+const headelement = document.head;
+console.log(headelement);
 
 
 
