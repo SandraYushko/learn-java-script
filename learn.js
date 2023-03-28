f7d791fab9078ef64e7674b00fbd5bc6d3fce153
SandraYushko <shyria6@gmail.com>
2023-03-28 22:38:41 +0300

    CodeWars 28.03.2023
    7 kyu
    Odd-Even String Sort

--- learn.js
+++ learn.js
@@ -1,5 +1,13 @@
-const headelement = document.head;
-console.log(headelement);
+function sortMyString(S) {
+  // your code here
+let neChetnie='';
+let chetnie='';
+for (let i=0; i<S.length; i++){
+  if (i%2 == 0) {chetnie += S[i]}
+  else {neChetnie += S[i]}
+}
+  return `${chetnie} ${neChetnie}`;
+}
 
 
 
