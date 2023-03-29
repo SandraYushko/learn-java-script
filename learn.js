046917652e4f7435bbbf4a7182e6466502f9fb6e
SandraYushko <shyria6@gmail.com>
2023-03-29 23:15:57 +0300

    CodeWars 29.03.2023
    7 kyu
    Sum of two lowest positive integers

--- learn.js
+++ learn.js
@@ -1,12 +1,7 @@
-function sortMyString(S) {
-  // your code here
-let neChetnie='';
-let chetnie='';
-for (let i=0; i<S.length; i++){
-  if (i%2 == 0) {chetnie += S[i]}
-  else {neChetnie += S[i]}
-}
-  return `${chetnie} ${neChetnie}`;
+function sumTwoSmallestNumbers(numbers) {  
+  //Code here
+  let numbersNew = numbers.sort(function(a,b){return a-b});
+  return numbersNew[0]+numbersNew[1];
 }
 
 
