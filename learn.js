d6c64b6c25b8c03efbf1a8a426a5ce2b02405c79
SandraYushko <shyria6@gmail.com>
2023-04-01 10:26:26 +0300

    CodeWars 01/04/2023
    7 kyu
    How many consecutive numbers are needed?

--- learn.js
+++ learn.js
@@ -1,8 +1,12 @@
-function sumTwoSmallestNumbers(numbers) {  
-  //Code here
-  let numbersNew = numbers.sort(function(a,b){return a-b});
-  return numbersNew[0]+numbersNew[1];
-}
+function consecutive(arr) {
+  //code me
+    let max = Math.max.apply(null,arr);
+    let min = Math.min.apply(null,arr);
+    if (arr.length > 2) return (max - min) - arr.length+1;
+        
+    else return 0;
+    //((arr.length = 0) || (arr.length = 1))
+  }
 
 
 
