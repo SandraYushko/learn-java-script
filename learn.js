e8731729ad1f3284845a67dd1582310b9aed9db9
SandraYushko <shyria6@gmail.com>
2023-04-03 20:05:13 +0300

    CodeWars 03/04/2023
    7 kyu
    Beginner Series #3 Sum of Numbers

--- learn.js
+++ learn.js
@@ -1,7 +1,11 @@
-function roundToNext5(n) {
-  // ...
-  return Math.ceil(n / 5) * 5;
-
+function getSum(a, b) {
+  //Good luck!
+  if (a == b) return a;
+  let sum = 0;
+  for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+    sum += i;
+  }
+  return sum;
 }
 
 
