7f00bba9699e3d4237107f1747ab3fd0f15f312b
SandraYushko <shyria6@gmail.com>
2023-04-13 20:04:35 +0300

    CodeWars 7 kyu Mumbling 13.04.2023

--- learn.js
+++ learn.js
@@ -1,13 +1,13 @@
-function getSum(a, b) {
-  //Good luck!
-  if (a == b) return a;
-  let sum = 0;
-  for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
-    sum += i;
-  }
-
-  return sum;
+function accum(s) {
+  // your code
+  return (s.split("").map((item, index) => (item.toUpperCase() + item.repeat(index).toLowerCase())).join('-'));
 }
+
+
+accum("ZpglnRxqenU");
+
+
+
 
 
 
