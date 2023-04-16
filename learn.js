db58328ebe6d775d4987663a6fe1b5677a54bc5c
SandraYushko <shyria6@gmail.com>
2023-04-16 12:56:42 +0300

    7 kyu Highest and Lowest 16.04.2023

--- learn.js
+++ learn.js
@@ -1,6 +1,7 @@
-function reverseList(list) {
-  return list.reverse()
+function highAndLow(numbers){
+  console.log(`${Math.max.apply(null, numbers.split(" "))} ${Math.min.apply(null, numbers.split(" "))}`);
 }
+highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4")
 
 
 
