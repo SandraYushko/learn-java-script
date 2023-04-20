2cdea9e9bf3872337f71f58cf69c0f5488b18f2c
SandraYushko <shyria6@gmail.com>
2023-04-20 22:44:42 +0300

    8 kyu Quadrants 20.04.2023

--- learn.js
+++ learn.js
@@ -1,7 +1,19 @@
-function friend(friends){
-  return friends.filter(function (item) { return item.length === 4 });
-}
+function quadrant(x, y) {
+  // Poveli!
+  if (x>=0 && y>=0) {
+    return 1
+  }
 
+  else if (x<0 && y>=0) {
+    return 2
+  }
+
+  else if (x>=0 && y<0) {
+    return 4
+  }
+
+  else return 3
+}
 
 
 
