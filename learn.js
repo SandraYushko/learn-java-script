21279d34193b1d4271312c6b67bacf05f694fa7d
SandraYushko <shyria6@gmail.com>
2023-04-23 00:12:43 +0300

    CodeWars 8 kyu Did she say hallo? 23.04.2023

--- learn.js
+++ learn.js
@@ -1,18 +1,6 @@
-function quadrant(x, y) {
-  // Poveli!
-  if (x>=0 && y>=0) {
-    return 1
-  }
+function validateHello(greetings) {
+  return /hello|ciao|salut|hallo|hola|ahoj|czesc/i.test(greetings)
 
-  else if (x<0 && y>=0) {
-    return 2
-  }
-
-  else if (x>=0 && y<0) {
-    return 4
-  }
-
-  else return 3
 }
 
 
