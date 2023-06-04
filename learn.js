8140c8af04f830b8e32b1b12fcd00ec2ffbe53d7
SandraYushko <shyria6@gmail.com>
2023-06-04 23:35:43 +0300

    8 kyu Name Shuffler 04/06/2023

--- learn.js
+++ learn.js
@@ -1,10 +1,6 @@
-function sumMix(x){
-  let result = x.reduce(function(sum,elem){
-    return sum+Number(elem);
-  },0);
-  return result;
+function nameShuffler(str){
+  return str.split(" ").reverse().join(" ")
 }
-
 
 
 
