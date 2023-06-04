fd001270160b10f1fc6e4471b9740f7fe77d5b64
SandraYushko <shyria6@gmail.com>
2023-06-04 23:47:38 +0300

    8 kyu Thinkful - Logic Drills: Traffic light 04/06/2023

--- learn.js
+++ learn.js
@@ -1,6 +1,11 @@
-function nameShuffler(str){
-  return str.split(" ").reverse().join(" ")
+function updateLight(current) {
+
+  if (current == "green") return "yellow"
+  if (current == "yellow") return "red"
+  if (current == "red") return "green"
+
 }
+updateLight("red")
 
 
 
