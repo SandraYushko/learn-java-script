a4cedd28872453d5af0e55c60cb99f9f3a73e503
SandraYushko <shyria6@gmail.com>
2023-06-07 22:53:33 +0300

    8 kyu Sum without highest and lowest number 07/06/2023

--- learn.js
+++ learn.js
@@ -1,12 +1,17 @@
-var min = function(list){
+function sumArray(array) {
+  if (array ===null || array ===[ ] || array ===undefined) return 0
+  else {
+    let newArray = array.sort(function(a,b){return a-b})
+    let sum = 0
+    for (let i=1; i< (newArray.length-1); i++){
+      sum += newArray[i]
+    }
+    return sum
+  }
 
-  return Math.min.apply(null, list);
 }
+sumArray([ 6, 2, 1, 8, 10 ])
 
-var max = function(list){
-
-  return Math.max.apply(null, list);
-}
 
 
 
