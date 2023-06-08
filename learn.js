96e4c16952d6577f0b4615b964b6f51eaf9ad347
SandraYushko <shyria6@gmail.com>
2023-06-08 21:23:19 +0300

    8 kyu Count the Monkeys! 07/06/2023

--- learn.js
+++ learn.js
@@ -1,16 +1,12 @@
-function sumArray(array) {
-  if (array ===null || array ===[ ] || array ===undefined) return 0
-  else {
-    let newArray = array.sort(function(a,b){return a-b})
-    let sum = 0
-    for (let i=1; i< (newArray.length-1); i++){
-      sum += newArray[i]
-    }
-    return sum
+function monkeyCount(n) {
+  let schet = []
+  for (let i=1; i<=n; i++) {
+    schet[i-1]=i
   }
-
+  return schet
 }
-sumArray([ 6, 2, 1, 8, 10 ])
+
+monkeyCount(3)
 
 
 
