4180e830b4c00e55fedac75c141b7260e05809e5
SandraYushko <shyria6@gmail.com>
2023-06-10 10:45:38 +0300

    8 kyu L1: Set Alarm 10/06/2023

--- learn.js
+++ learn.js
@@ -1,14 +1,12 @@
-function firstNonConsecutive (arr) {
-  for (let i=0; i<arr.length-1; i++){
+function setAlarm(employed, vacation){
+  if (employed && vacation) return false
+  else if (!employed && vacation) return false
+  else if (!employed && !vacation) return false
+  else if (employed && !vacation) return true
 
-    if (arr[i+1] != arr[i]+1) {
-      return arr[i+1]}
-
-  }
-  return null
 }
 
-firstNonConsecutive([1,2,3,4])
+setAlarm(true, false)
 
 
 
