99e55edae86c43a890f7a223ea5b49d8d6e17f15
SandraYushko <shyria6@gmail.com>
2023-06-14 21:45:18 +0300

    8 kyu Area or Perimeter 14/06/2023

--- learn.js
+++ learn.js
@@ -1,11 +1,11 @@
-function feast(beast, dish) {
+const areaOrPerimeter = function(l , w) {
+  // Return your answer
+  if (l == w) return l*w
+  else return (l+w)*2
+};
 
-  if ((beast[0] == dish[0])  &&  (beast[beast.length-1] == dish[dish.length-1])) {return true}
-  return false
-}
-
-feast("chickadee", "chocolate cake")
-feast("brown bear", "bear claw")
+areaOrPerimeter(3,  3)
+areaOrPerimeter(6, 10)
 
 
 
