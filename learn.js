ccf526b2aa4099b1ffda97fa8e579e2502ddca8d
SandraYushko <shyria6@gmail.com>
2023-07-01 23:18:56 +0300

    8 kyu Sum The Strings 01/07/2023

--- learn.js
+++ learn.js
@@ -1,12 +1,6 @@
-function strCount(str, letter){
-    let sum=0
-    for (let i=0; i<str.length; i++) {
-        if (str[i]===letter) ++sum
-    }
-    return sum
+function sumStr(a,b) {
+    return String(Number(a)+Number(b))
 }
-
-
 
 
 
