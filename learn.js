095c878e025d7f94dfcf4a8c5a91b7dafafd4b9c
SandraYushko <shyria6@gmail.com>
2023-07-05 23:51:07 +0300

    8 kyu I love you, a little , a lot, passionately ... not at all 05/07/2023

--- learn.js
+++ learn.js
@@ -1,5 +1,7 @@
-function sumStr(a,b) {
-    return String(Number(a)+Number(b))
+function howMuchILoveYou(nbPetals) {
+    let phrase = ["I love you", "a little", "a lot", "passionately", "madly", "not at all"]
+    if (nbPetals%6===0) {return phrase[5]}
+    else return phrase[nbPetals%6-1]
 }
 
 
