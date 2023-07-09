2e8e0e3c137b95e65b74bd65c837475530eb5119
SandraYushko <shyria6@gmail.com>
2023-07-09 22:05:00 +0300

    8 kyu Student's Final Grade 09/07/2023

--- learn.js
+++ learn.js
@@ -1,9 +1,8 @@
-function powersOfTwo(n){
-    let massiv=[]
-    for (let i=0; i<=n; i++){
-        massiv.push(Math.pow(2, i))
-    }
-    return massiv
+function finalGrade (exam, projects) {
+    if (exam>90 || projects>10) return 100
+    else if (exam>75 && projects>=5) return 90
+    else if (exam>50 && projects>=2) return 75
+    else return 0
 }
 
 
