c46d84c7dbd2b157c26e8a341b6e3500f1095445
SandraYushko <shyria6@gmail.com>
2023-07-14 22:54:06 +0300

    8 kyu Who ate the cookie? 14/07/2023

--- learn.js
+++ learn.js
@@ -1,7 +1,7 @@
-function noBoringZeros(n) {
-
-    while (n%10 == 0 && n!=0) {n/=10}
-    return n
+function cookie(x){
+    if (typeof x==="string") {return "Who ate the last cookie? It was Zach!"}
+    else if (typeof x==="number") {return "Who ate the last cookie? It was Monica!"}
+    else {return "Who ate the last cookie? It was the dog!"}
 }
 
 
