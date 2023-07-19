28adba1f196b9231fed13067a8cedb6912f7713d
SandraYushko <shyria6@gmail.com>
2023-07-19 22:27:20 +0300

    8 kyu Do I get a bonus? 19/07/2023

--- learn.js
+++ learn.js
@@ -1,7 +1,6 @@
-function cookie(x){
-    if (typeof x==="string") {return "Who ate the last cookie? It was Zach!"}
-    else if (typeof x==="number") {return "Who ate the last cookie? It was Monica!"}
-    else {return "Who ate the last cookie? It was the dog!"}
+function bonusTime(salary, bonus) {
+    if (bonus===true) {return `£${salary*10}`}
+    else {return `£${salary}`}
 }
 
 
