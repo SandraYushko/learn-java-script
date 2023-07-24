80116eaf2f7507228b1ef9afe651a9a2c1f2ccde
SandraYushko <shyria6@gmail.com>
2023-07-24 23:08:25 +0300

    8 kyu Find numbers which are divisible by given number 24/07/2023

--- learn.js
+++ learn.js
@@ -1,6 +1,9 @@
-function bonusTime(salary, bonus) {
-    if (bonus===true) {return `£${salary*10}`}
-    else {return `£${salary}`}
+function divisibleBy(numbers, divisor){
+    let result=[];
+    for (let i=0; i<numbers.length; i++) {
+        if (numbers[i]%divisor == 0) result.push(numbers[i])
+    }
+    return result
 }
 
 
