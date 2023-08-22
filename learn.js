6540f380b481f143ba4b491edd8b2ab5af0995b0
SandraYushko <shyria6@gmail.com>
2023-08-22 20:16:59 +0300

    8 kyu Name on billboard
    23/08/2023

--- learn.js
+++ learn.js
@@ -1,19 +1,7 @@
-function isOpposite(s1,s2){
-    if ((s1=='') || (s2=='')) return false
-
-
-    let new_s1 = "";
-    for (let i = 0; i < s1.length; i++) {
-        if (s1[i] === s1[i].toLowerCase()) {
-            new_s1 += s1[i].toUpperCase();
-        } else {
-            new_s1 += s1[i].toLowerCase();
-        }
-    }
-
-
-    return (new_s1 === s2)
-
+function billboard(name, price = 30){
+    let summ=0;
+    for (let i=0; i<name.length; i++) {summ+=price}
+    return summ
 }
 
 
