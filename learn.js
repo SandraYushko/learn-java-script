c3137e80e3ad693055f646cbeb9cb24643c07158
SandraYushko <shyria6@gmail.com>
2023-08-22 06:29:42 +0300

    8 kyu They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it. This is a sadly story #1: Are they opposite?
    22/08/2023

--- learn.js
+++ learn.js
@@ -1,9 +1,19 @@
-function findMultiples(integer, limit) {
-    let massiv=[]
-    for (let i=integer; i<=limit; i+=integer){
-        massiv.push(i)
+function isOpposite(s1,s2){
+    if ((s1=='') || (s2=='')) return false
+
+
+    let new_s1 = "";
+    for (let i = 0; i < s1.length; i++) {
+        if (s1[i] === s1[i].toLowerCase()) {
+            new_s1 += s1[i].toUpperCase();
+        } else {
+            new_s1 += s1[i].toLowerCase();
+        }
     }
-    return massiv
+
+
+    return (new_s1 === s2)
+
 }
 
 
