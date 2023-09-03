98cd40aadad697858e5c8d5bfadbdcfa621aef26
SandraYushko <shyria6@gmail.com>
2023-09-03 22:57:29 +0300

    8 kyu Determine offspring sex based on genes XX and XY chromosomes
    03/09/2023

--- learn.js
+++ learn.js
@@ -1,5 +1,6 @@
-function distinct(a) {
-    return [...new Set(a)]
+function chromosomeCheck(sperm) {
+    if (sperm === 'XY') return "Congratulations! You're going to have a son."
+    else return "Congratulations! You're going to have a daughter."
 }
 
 
