1b1e8de6632b5e297ee187ff08aa8d1ff0f92b0f
SandraYushko <shyria6@gmail.com>
2023-09-04 22:33:12 +0300

    8 kyu Incorrect division method
    04/09/2023

--- learn.js
+++ learn.js
@@ -1,6 +1,3 @@
-function chromosomeCheck(sperm) {
-    if (sperm === 'XY') return "Congratulations! You're going to have a son."
-    else return "Congratulations! You're going to have a daughter."
-}
+const solve = (x, y) => x /y
 
 
