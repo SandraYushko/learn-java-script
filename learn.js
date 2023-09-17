a62bc11c75d2e63e7b1bdb840cc834af5320df24
SandraYushko <shyria6@gmail.com>
2023-09-18 00:04:46 +0300

    home work from https://react.dev/
    18/09/2023

--- learn.js
+++ learn.js
@@ -1,5 +1,46 @@
-function getASCII(c){
-    return c.charCodeAt(0)
+import { people } from './data.js';
+import { getImageUrl } from './utils.js';
+
+export default function List() {
+    const chemists = people.filter(person => person.profession === 'chemist');
+    const everyoneElse = people.filter(person =>
+        person.profession !== 'chemist'
+    );
+    return (
+        <article>
+            <h1>Chemists</h1>
+            <ul>{chemists.map(person =>
+                <li key={person.id}>
+                    <img
+                        src={getImageUrl(person)}
+                        alt={person.name}
+                    />
+                    <p>
+                        <b>{person.name}:</b>
+                        {' ' + person.profession + ' '}
+                        known for {person.accomplishment}
+                    </p>
+                </li>
+            )}
+            </ul>
+
+            <h1>Everyone Else</h1>
+            <ul>{everyoneElse.map(person =>
+                <li key={person.id}>
+                    <img
+                        src={getImageUrl(person)}
+                        alt={person.name}
+                    />
+                    <p>
+                        <b>{person.name}:</b>
+                        {' ' + person.profession + ' '}
+                        known for {person.accomplishment}
+                    </p>
+                </li>
+            )}
+            </ul>
+        </article>
+    );
 }
 
 
