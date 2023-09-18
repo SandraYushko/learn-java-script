f91f78de534c7bb6dd0f9e5bfa3e4bf5d597979f
SandraYushko <shyria6@gmail.com>
2023-09-18 23:34:02 +0300

    home work from https://react.dev/   (State: A Component's Memory)
    18/09/2023

--- learn.js
+++ learn.js
@@ -1,28 +1,34 @@
-import { recipes } from './data.js';
+import { sculptureList } from './data.js';
+import { useState } from 'react';
 
-function Recipe({ id, name, ingredients }) {
-    return (
-        <div>
-            <h2>{name}</h2>
-            <ul>
-                {ingredients.map(ingredient =>
-                    <li key={ingredient}>
-                        {ingredient}
-                    </li>
-                )}
-            </ul>
-        </div>
-    );
-}
+export default function Gallery() {
 
-export default function RecipeList() {
+    [index, setNewindex] = useState(0)
+    function handleClick() {
+        setNewindex(index + 1)
+    }
+
+    let sculpture = sculptureList[index];
     return (
-        <div>
-            <h1>Recipes</h1>
-            {recipes.map(recipe =>
-                <Recipe {...recipe} key={recipe.id} />
-            )}
-        </div>
+        <>
+            <button onClick={handleClick}>
+                Next
+            </button>
+            <h2>
+                <i>{sculpture.name} </i>
+                by {sculpture.artist}
+            </h2>
+            <h3>
+                ({index + 1} of {sculptureList.length})
+            </h3>
+            <img
+                src={sculpture.url}
+                alt={sculpture.alt}
+            />
+            <p>
+                {sculpture.description}
+            </p>
+        </>
     );
 }
 
