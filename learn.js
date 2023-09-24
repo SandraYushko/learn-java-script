0077cf30c01ad971edd6eca6b14c6321c6acf2ad
SandraYushko <shyria6@gmail.com>
2023-09-24 20:23:22 +0300

    home work from https://react.dev/   (Responding to Events) 2 24/09/2023

--- learn.js
+++ learn.js
@@ -1,16 +1,12 @@
-export default function LightSwitch() {
-    function handleClick() {
-        let bodyStyle = document.body.style;
-        if (bodyStyle.backgroundColor === 'black') {
-            bodyStyle.backgroundColor = 'white';
-        } else {
-            bodyStyle.backgroundColor = 'black';
-        }
-    }
-
+export default function ColorSwitch({
+                                        onChangeColor
+                                    }) {
     return (
-        <button onClick={handleClick}>
-            Toggle the lights
+        <button onClick={(e) => {
+            e.stopPropagation();
+            onChangeColor();
+        }}>
+            Change color
         </button>
     );
 }
