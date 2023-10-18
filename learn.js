d253cd3b15b4a3dc0301b6a0a4318d551c29b3f2
SandraYushko <shyria6@gmail.com>
2023-10-18 22:58:39 +0300

    8 kyu Neutralisation 18/10/2023

--- learn.js
+++ learn.js
@@ -1,4 +1,13 @@
-const fixTheMeerkat = (arr) => arr.reverse()
+function neutralise(s1, s2) {
+    let s = ''
+    for (let i = 0; i < s1.length; i++) {
+        if (s1[i] == s2[i]) {
+            s = s+s1[i]
+        }
+        else s = s+'0'
+    }
+    return s
+}
 
 
 
