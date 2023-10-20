cd2f8204c12a14b16d1a17382441b31d7bb36f7f
SandraYushko <shyria6@gmail.com>
2023-10-20 23:21:15 +0300

    8 kyu (2) Simple Fun #261: Whose Move 20/10/2023

--- learn.js
+++ learn.js
@@ -1,8 +1,4 @@
-function whoseMove(lastPlayer, win) {
-    if (lastPlayer==="black" && win===false) {return "white"}
-    else if (lastPlayer==="white" && win===false) {return "black"}
-    else if (win===true) {return lastPlayer}
-}
+const whoseMove = (lastPlayer, win) => win ? lastPlayer : lastPlayer==="white" ? "black" : "white"
 
 
 
