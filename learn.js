9d42a089054f58ec3e70fbad18c8811f1701e02d
SandraYushko <shyria6@gmail.com>
2023-10-22 23:28:47 +0300

    8 kyu Flick Switch 22/10/2023

--- learn.js
+++ learn.js
@@ -1,4 +1,10 @@
-const whoseMove = (lastPlayer, win) => win ? lastPlayer : lastPlayer==="white" ? "black" : "white"
+function flickSwitch(arr){
+    let start = true
+    return arr.map(el => {
+        if (el === 'flick') return start=!start
+        else return start
+    } )
+}
 
 
 
