0a65c154e2a7c8e123ed5071075f7369c1bb384f
SandraYushko <shyria6@gmail.com>
2023-10-24 22:27:51 +0300

    8 kyu (2) Collatz Conjecture (3n+1) 24/10/2023

--- learn.js
+++ learn.js
@@ -1,4 +1,11 @@
-const hexToDec = (hexString) => parseInt(hexString, 16)
+var hotpo = function(n) {
+    let num = 0
+    while (n>1) {
+      num++;
+      n=  n % 2 ? 3*n + 1 : n/2;
+    }
+    return num;
+}
 
 
 
