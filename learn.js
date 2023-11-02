42eb130b4bd4008e0ec8e1897e8f7d3e07354848
SandraYushko <shyria6@gmail.com>
2023-11-02 23:02:23 +0300

    8 kyu Merging sorted integer arrays (without duplicates) 02/11/2023

--- learn.js
+++ learn.js
@@ -1,11 +1,7 @@
-var hotpo = function(n) {
-    let num = 0
-    while (n>1) {
-      num++;
-      n=  n % 2 ? 3*n + 1 : n/2;
-    }
-    return num;
+function mergeArrays(a, b) {
+    return [...new Set(a.concat(b).sort((c,d)=>c-d))]
 }
+
 
 
 
