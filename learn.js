90e1ea34e02f176abbd9c1e22833b975c4db926c
SandraYushko <shyria6@gmail.com>
2023-11-12 22:18:03 +0300

    8 kyu (2) Are there any arrows left? 12/11/2023

--- learn.js
+++ learn.js
@@ -1,9 +1,4 @@
-function anyArrows(arrows){
-    // arrow it
-    let newArr = arrows.filter(a => a.damaged !== true);
-    if (newArr.length>0) return true
-    else return false
-}
+let anyArrows = (arrows) => arrows.some(a => !a.damaged)
 
 
 
