491be511b0c6fef41883118f2f6e9bfa4d68daea
SandraYushko <shyria6@gmail.com>
2023-11-28 22:14:57 +0300

    8 kyu Job Matching #1 28/11/2023

--- learn.js
+++ learn.js
@@ -1,4 +1,8 @@
-let printArray = (array) => array.toString()
+function match(candidate, job) {
+    if (!candidate.minSalary || !job.maxSalary)
+        throw error
+    return candidate.minSalary*0.9 <= job.maxSalary
+}
 
 
 
