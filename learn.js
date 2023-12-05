9878b6e8a383c910a470cb628ac58a38aa4c8ac1
SandraYushko <shyria6@gmail.com>
2023-12-05 23:13:03 +0300

    8 kyu Triple Trouble 05/12/2023

--- learn.js
+++ learn.js
@@ -1,4 +1,10 @@
-const whoIsPaying = (name) => name.length >2 ? [name, name.substr(0,2)] : [name]
+function tripleTrouble(one, two, three){
+    let str = one[0]+two[0]+three[0]
+    for (let i=1; i<one.length; i++) {
+        str += one[i]+two[i]+three[i]
+    }
+    return str
+}
 
 
 
