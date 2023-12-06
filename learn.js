b5205c55c9946468c673a89b640183402bf1e02b
SandraYushko <shyria6@gmail.com>
2023-12-06 23:17:12 +0300

    8 kyu (2) Points of Reflection 06/12/2023

--- learn.js
+++ learn.js
@@ -1,10 +1,4 @@
-function symmetricPoint(p, q) {
-    const dx = q[0] - p[0];
-    const dy = q[1] - p[1];
-    const x = q[0] + dx;
-    const y = q[1] + dy;
-    return [x, y];
-}
+let symmetricPoint = (p, q) => [2*q[0]-p[0], 2*q[1]-p[1]]
 
 
 
