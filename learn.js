3a3697781b1daff41611cbe4b2d074d2a5c0ac6d
SandraYushko <shyria6@gmail.com>
2023-12-12 19:55:47 +0300

    8 kyu Compare within margin 12/12/2023

--- learn.js
+++ learn.js
@@ -1,6 +1,11 @@
-function htmlspecialchars(formData) {
-    formData = formData.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;");
-    return formData;
+function closeCompare(a, b, margin = 0) {
+    if (Math.abs(a - b) <= margin) {
+        return 0;
+    } else if (a < b) {
+        return -1;
+    } else {
+        return 1;
+    }
 }
 
 
