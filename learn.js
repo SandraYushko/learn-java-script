f86214829ee0c341535abe674be5ec2960f12b26
SandraYushko <shyria6@gmail.com>
2024-01-01 23:35:26 +0300

    8 kyu How old will I be in 2099? 01/01/2024

--- learn.js
+++ learn.js
@@ -1,9 +1,10 @@
-var cubeChecker = function(volume, side){
-    if (volume>0||side>0) {
-        return side*side*side === volume ? true : false
-    }
-    return false
-};
+let calculateAge = (b, q) => {
+    if (b===q) return "You were born this very year!"
+    else if (q-b===1) return 'You are 1 year old.'
+    else if (b-q===1) return 'You will be born in 1 year.'
+    else if (b<q) return `You are ${q-b} years old.`
+    else if (b>q) return `You will be born in ${b-q} years.`
+}
 
 
 
