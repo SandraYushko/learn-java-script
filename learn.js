d083684490304da18620c909e84ba44b682d40ec
SandraYushko <shyria6@gmail.com>
2024-01-03 23:43:01 +0300

    8 kyu How many stairs will Suzuki climb in 20 years? 03/01/2024

--- learn.js
+++ learn.js
@@ -1,10 +1,4 @@
-let calculateAge = (b, q) => {
-    if (b===q) return "You were born this very year!"
-    else if (q-b===1) return 'You are 1 year old.'
-    else if (b-q===1) return 'You will be born in 1 year.'
-    else if (b<q) return `You are ${q-b} years old.`
-    else if (b>q) return `You will be born in ${b-q} years.`
-}
+let stairsIn20 = (s) => s.reduce((a,b)=>a+b.reduce((c,e)=>c+e,0),0)*20
 
 
 
