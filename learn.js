d818ca4bf20f308a12c9508c1c022d36a4f37912
SandraYushko <shyria6@gmail.com>
2024-01-13 20:44:20 +0300

    8 kyu Sort My Textbooks 13/01/2024

--- learn.js
+++ learn.js
@@ -1,8 +1,7 @@
-function warnTheSheep(queue) {
-    for (let i=0; i<queue.length; i++) {
-        if (queue[queue.length-1]==="wolf") {return "Pls go away and stop eating my sheep"}
-        if (queue[i]==="wolf") {return `Oi! Sheep number ${queue.length-i-1}! You are about to be eaten by a wolf!`}
-    }
+function sorter(textbooks) {
+
+    return textbooks.sort((a, b) => a.toLowerCase() > b.toLowerCase() ? 1 :
+        a.toLowerCase() < b.toLowerCase() ? -1  : 0)
 }
 
 
