2da59fea8b2b5020b38c835d3992b5afd5e1f6ed
SandraYushko <shyria6@gmail.com>
2024-01-16 19:22:59 +0300

    8 kyu (2) L1: Bartender, drinks! 16/01/2024

--- learn.js
+++ learn.js
@@ -1,12 +1,15 @@
-function getDrinkByProfession(param){
-    let newParam=param.toLowerCase()
-    if (newParam === "jabroni") {return "Patron Tequila"}
-    if (newParam === "school counselor") {return "Anything with Alcohol"}
-    if (newParam === "programmer") {return "Hipster Craft Beer"}
-    if (newParam === "bike gang member") {return "Moonshine"}
-    if (newParam === "politician") {return "Your tax dollars"}
-    if (newParam === "rapper") {return "Cristal"}
-    else {return "Beer"}
+function getDrinkByProfession(param) {
+    param = param.toLowerCase();
+
+    switch(param) {
+        case "jabroni": return "Patron Tequila";
+        case "school counselor": return "Anything with Alcohol";
+        case "programmer": return "Hipster Craft Beer";
+        case "bike gang member": return "Moonshine";
+        case "politician": return	"Your tax dollars";
+        case "rapper": return "Cristal";
+        default: return "Beer";
+    }
 }
 
 
