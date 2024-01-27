08f9fc6dacc510f19f390d09ac2ce6e09aeac359
SandraYushko <shyria6@gmail.com>
2024-01-27 20:27:44 +0300

    precurs it-incubator JS (massiv) 27/01/2024

--- learn.js
+++ learn.js
@@ -2,7 +2,8 @@
 const info = {
     title: 'it-incubator',
     graduatesCount: 2000,
-    areYouChampion: true
+    areYouChampion: true,
+    technologies: ['Front', 'Back', 'DevOps']
 }
 
 // render
@@ -18,6 +19,21 @@ let areYouChampionElement = document.createElement('input');
 areYouChampionElement.type = 'checkbox';
 areYouChampionElement.checked = info.areYouChampion;
 document.body.append(areYouChampionElement);
+
+
+
+let selectElement = document.createElement('select');
+let option0Element = document.createElement('option');
+option0Element.append(info.technologies[0])
+
+let option1Element = document.createElement('option');
+option1Element.append(info.technologies[1])
+
+let option2Element = document.createElement('option');
+option2Element.append(info.technologies[2])
+selectElement.append(option0Element, option1Element, option2Element)
+
+document.body.append(selectElement);
 
 
 
